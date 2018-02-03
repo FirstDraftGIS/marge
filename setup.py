9fb9d4a06ca9502e2b5bfed3e61f2c8823c4d6f6
DanielJDufour <daniel.j.dufour@gmail.com>
2018-02-03 02:29:35 +0000

    added setup.py and added pandas to reqs

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'marge',
+  packages = ['marge'],
+  package_dir = {'marge': 'marge'},
+  package_data = {'marge': ['__init__.py']},
+  version = '0.1',
+  description = 'Model that Automatically Resolves Geographic Entities',
+  author = 'Daniel J. Dufour',
+  author_email = 'daniel.j.dufour@gmail.com',
+  url = 'https://github.com/DanielJDufour/marge',
+  download_url = 'https://github.com/DanielJDufour/marge/tarball/download',
+  keywords = ['location','geo','python','tensorflow'],
+  classifiers = [],
+  install_requires=["numpy, pandas, scipy, tensorflow"]
+)
