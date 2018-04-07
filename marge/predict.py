61c75ceee082c63cbae267b25eda02b68b60c60a
DanielJDufour <daniel.j.dufour@gmail.com>
2018-04-07 18:55:00 +0000

    added excp catching

--- marge/predict.py
+++ marge/predict.py
@@ -8,13 +8,26 @@ def get_model():
         return pickle.load(f)
 
 def get_probabilities(X, model=None):
+    
+    try:
+    
+        print("starting get probabilities with", X)
+    
+        if model is None:
+            model = get_model()
 
-    if model is None:
-        model = get_model()
+        num_actual_columns = len(X.columns)
+        num_expected_columns = len(model.coef_)
+        if num_actual_columns != num_expected_columns:
+            raise Exception("[marge] expected " + str(num_expected_columns) + " columns but got " + str(num_actual_columns) + " instead.")
+    
+    
+        get_probabilities = model.predict(X)
+    
+        return get_probabilities
 
-    get_probabilities = model.predict(X)
-
-    return get_probabilities
+    except Exception as e:
+        print("[marge] get_probabilities hit an error:", e)
 
 def select(X, model=None):
 
