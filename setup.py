0ded5a3b2c25e888c91fd97e424d51b69e398166
DanielJDufour <daniel.j.dufour@gmail.com>
2018-04-28 17:50:38 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'marge',
   packages = ['marge'],
   package_dir = {'marge': 'marge'},
-  package_data = {'marge': ['__init__.py']},
+  package_data = {'marge': ['__init__.py', 'cleaner.py', 'config.py', 'config.yml', 'converter.py', 'enricher.py', 'enumerations.py', 'models.py', 'resolver.py', 'split.py', 'train.py', 'utils.py','configs/__init__.py', 'configs/first_pass.py', 'configs/second_pass.py', 'tests/__init__.py', 'tests/cleaner.py', 'tests/models.py', 'tests/utils.py']},
   version = '0.1',
   description = 'Model that Automatically Resolves Geographic Entities',
   author = 'Daniel J. Dufour',
